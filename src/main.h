b999258b3540de54bbe519425ee460869704a26b
Pat <raithofer@paralab-consulting.de>
2020-06-07 16:13:44 +0200

    bump fork block and version

--- src/main.h
+++ src/main.h
@@ -36,10 +36,10 @@ static const int64_t MIN_TX_FEE = 0.1 * COIN;
 static const int64_t MIN_RELAY_TX_FEE = MIN_TX_FEE;
 static const int64_t MAX_MONEY = 500000000 * COIN;
 static const int64_t COIN_YEAR_REWARD = 200 * CENT;
-// implement consensus vote result Option B. Set Max Supply to 3000M RAIN and staking to 10%
+// implement consensus vote result Option B. Set Max Supply to 4000M RAIN and staking to 10%
 static const int64_t COIN_YEAR_REWARD_NEW = 10 * CENT;
-static const int64_t RAIN_CAP = 3000000000 * COIN;
-static const int CONSENSUS_CHANGE_BLOCK = 1743040; // should be around June 11th 2020
+static const int64_t RAIN_CAP = 4000000000 * COIN;
+static const int CONSENSUS_CHANGE_BLOCK = 1800000; // should be around July 20th 2020
 
 inline bool MoneyRange(int64_t nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 // Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp.
