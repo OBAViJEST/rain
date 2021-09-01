160fd6ff348c50fd4702865bafa221c59ba15ad1
Raipat <raipat@web.de>
2021-09-01 20:43:35 +0200

    Bump fork block to March 2022

--- src/main.h
+++ src/main.h
@@ -38,8 +38,8 @@ static const int64_t MAX_MONEY = 500000000 * COIN;
 static const int64_t COIN_YEAR_REWARD = 200 * CENT;
 // implement consensus vote result Option B. Set Max Supply and staking to 10%
 static const int64_t COIN_YEAR_REWARD_NEW = 10 * CENT;
-static const int64_t RAIN_CAP = 20000000000 * COIN;
-static const int CONSENSUS_CHANGE_BLOCK = 2389713; // should be around September 4th 2021
+static const int64_t RAIN_CAP = 40000000000 * COIN;
+static const int CONSENSUS_CHANGE_BLOCK = 2655956; // should be around March 5th 2022
 
 inline bool MoneyRange(int64_t nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 // Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp.
